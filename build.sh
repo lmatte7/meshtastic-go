cf17e65368678a4384e90f85fe8c16bc1731ad8a
Lucas Matte <lmatte7@gmail.com>
2021-05-20 12:11:25 -0500

    Added build script

---
+++ build.sh
@@ -0,0 +1,4 @@
+env GOOS=darwin GOARCH=amd64 go build -o builds/meshtastic_go_osx github.com/lmatte7/meshtastic-go
+env GOOS=linux GOARCH=arm GOARM=5 go build -o builds/meshtastic_go_linux_arm github.com/lmatte7/meshtastic-go
+env GOOS=linux GOARCH=arm64 go build -o builds/meshtastic_go_linux_arm64 github.com/lmatte7/meshtastic-go
+env GOOS=windows go build -o builds github.com/lmatte7/meshtastic-go
