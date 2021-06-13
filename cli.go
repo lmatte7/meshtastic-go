8f2e972482c29ffb8fef6f76dd0c073c1fd98cc0
Lucas Matte <lmatte@atlassian.com>
2021-06-12 19:34:40 -0500

    Added all info commands

--- cli.go
+++ cli.go
@@ -23,7 +23,12 @@ Subcommands
 
 Info
 info - show all info
+info:channels
 info:nodes - show nodes
+
+
+messages - display any stored messages
+messages:wait - wait for new messages
 
 Position
 position - show position
@@ -85,18 +90,31 @@ func Init() {
 				UsageText:   "info [command] - Show radio information",
 				Description: "Show node, preference and channel information for radio",
 				ArgsUsage:   "",
+				Action:      showAllRadioInfo,
 				Subcommands: []*cli.Command{
 					{
-						Name:    "all",
-						Aliases: []string{"a"},
-						Usage:   "Show all radio information",
-						Action:  showAllRadioInfo,
+						Name:    "radio",
+						Aliases: []string{"r"},
+						Usage:   "Show radio information",
+						Action:  showRadioInfo,
 					},
 					{
 						Name:    "channels",
 						Aliases: []string{"c"},
 						Usage:   "Show all channel information",
-						Action:  displayChannelInfo,
+						Action:  showChannelInfo,
+					},
+					{
+						Name:    "nodes",
+						Aliases: []string{"n"},
+						Usage:   "Show all nodes on the mesh",
+						Action:  showNodeInfo,
+					},
+					{
+						Name:    "preferences",
+						Aliases: []string{"p"},
+						Usage:   "Show radio user preferences",
+						Action:  showRadioPreferences,
 					},
 				},
 			},
@@ -113,124 +131,6 @@ func Init() {
 	}
 
 	app.Run(os.Args)
-
-	// fmt.Println(app)
-
-	// var port string
-	// var message string
-	// var owner string
-	// var setKey string
-	// var setValue string
-	// var url string
-	// var to int64
-
-	// flag.StringVar(&port, "port", "", "--port=port The serial port for the radio (Required)")
-	// flag.StringVar(&message, "text", "", "--text=message Send a text message")
-	// flag.StringVar(&url, "url", "", "--url=channel_curl Set the radio URL")
-	// flag.StringVar(&setKey, "setKey", "", "--setKey=key The key to set for a custom user preference option. Used with setValue")
-	// flag.StringVar(&setValue, "setValue", "", "--setValue=value The value to set for a custom user preference option. Used with setKey")
-	// flag.StringVar(&owner, "setOwner", "", "--setowner=owner Set the listed owner for the radio")
-	// flag.Int64Var(&to, "to", 0, "--to=destination Node to receive text (Used with sendtext)")
-	// recv := flag.Bool("recv", false, "Wait for new messages")
-	// infoPtr := flag.Bool("info", false, "Display radio information")
-	// longslowPtr := flag.Bool("longSlow", false, "Set long-range but slow channel")
-	// shortFast := flag.Bool("shortFast", false, "Set short-range but fast channel")
-
-	// flag.Usage = func() {
-	// 	flagSet := flag.CommandLine
-	// 	fmt.Printf("A command line tool for interacting with meshtastic radios\n")
-	// 	fmt.Printf("\n")
-	// 	fmt.Printf("USAGE\n")
-	// 	fmt.Printf("meshtastic-go -p <port> [COMMAND]\n")
-	// 	fmt.Printf("\n")
-	// 	fmt.Printf("COMMANDS\n")
-	// 	fmt.Printf("\n")
-	// 	order := []string{"port", "info", "recv", "longSlow", "shortFast", "text", "setOwner", "setKey", "setValue"}
-	// 	for _, name := range order {
-	// 		flag := flagSet.Lookup(name)
-	// 		fmt.Printf("--%-15s", flag.Name)
-	// 		fmt.Printf("%-10s\n", flag.Usage)
-	// 	}
-	// }
-
-	// flag.Parse()
-
-	// if port == "" {
-	// 	flag.Usage()
-	// 	os.Exit(1)
-	// }
-
-	// radio := gomesh.Radio{}
-	// if len(message) > 0 || *infoPtr || *recv || len(owner) > 0 || len(url) > 0 || *longslowPtr || *shortFast || len(setKey) > 0 || len(setValue) > 0 {
-	// 	radio.Init(port)
-	// 	defer radio.Close()
-	// }
-
-	// if message != "" {
-	// 	err := radio.SendTextMessage(message, to)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	}
-	// }
-
-	// if *recv {
-	// 	err := getRecievedMessages(radio)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	}
-	// }
-
-	// if *infoPtr {
-	// 	err := getRadioInfo(radio)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	}
-	// }
-
-	// if *longslowPtr {
-	// 	err := radio.SetChannel(0)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	} else {
-	// 		fmt.Printf("Set channel\n")
-	// 	}
-	// }
-
-	// if *shortFast {
-	// 	err := radio.SetChannel(1)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	} else {
-	// 		fmt.Printf("Set channel\n")
-	// 	}
-	// }
-
-	// if url != "" {
-	// 	err := radio.SetChannelURL(url)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	} else {
-	// 		fmt.Printf("Set URL successful\n")
-	// 	}
-	// }
-
-	// if owner != "" {
-	// 	err := radio.SetRadioOwner(owner)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	} else {
-	// 		fmt.Printf("Set owner successful\n")
-	// 	}
-	// }
-
-	// if setValue != "" && setKey != "" {
-	// 	err := radio.SetUserPreferences(setKey, setValue)
-	// 	if err != nil {
-	// 		fmt.Printf("ERROR: %s\n", err)
-	// 	} else {
-	// 		fmt.Printf("Set user preferences successful\n")
-	// 	}
-	// }
 
 }
 
@@ -269,7 +169,8 @@ func showAllRadioInfo(c *cli.Context) error {
 
 }
 
-func displayChannelInfo(c *cli.Context) error {
+func showNodeInfo(c *cli.Context) error {
+
 	radio := gomesh.Radio{}
 	err := radio.Init(c.String("port"))
 	if err != nil {
@@ -277,7 +178,49 @@ func displayChannelInfo(c *cli.Context) error {
 	}
 	defer radio.Close()
 
-	err = printChannelSettings(radio)
+	err = displayNodes(radio)
+	if err != nil {
+		return err
+	}
+
+	return nil
+
+}
+
+func showRadioInfo(c *cli.Context) error {
+
+	radio := gomesh.Radio{}
+	err := radio.Init(c.String("port"))
+	if err != nil {
+		return err
+	}
+	defer radio.Close()
+
+	responses, err := radio.GetRadioInfo()
+	if err != nil {
+		return err
+	}
+
+	for _, response := range responses {
+
+		if info, ok := response.GetPayloadVariant().(*gomeshproto.FromRadio_MyInfo); ok {
+			printRadioInfo(info)
+		}
+	}
+
+	return nil
+
+}
+
+func showRadioPreferences(c *cli.Context) error {
+	radio := gomesh.Radio{}
+	err := radio.Init(c.String("port"))
+	if err != nil {
+		return err
+	}
+	defer radio.Close()
+
+	err = printRadioPreferences(radio)
 	if err != nil {
 		return err
 	}
@@ -310,6 +253,24 @@ func displayChannelInfo(c *cli.Context) error {
 
 // }
 
+func displayNodes(r gomesh.Radio) error {
+	responses, err := r.GetRadioInfo()
+	if err != nil {
+		return err
+	}
+
+	nodes := make([]*gomeshproto.FromRadio_NodeInfo, 0)
+	for _, response := range responses {
+		if nodeInfo, ok := response.GetPayloadVariant().(*gomeshproto.FromRadio_NodeInfo); ok {
+			nodes = append(nodes, nodeInfo)
+		}
+	}
+
+	printNodes(nodes)
+
+	return nil
+}
+
 func getRadioInfo(r gomesh.Radio) error {
 
 	responses, err := r.GetRadioInfo()
@@ -322,55 +283,9 @@ func getRadioInfo(r gomesh.Radio) error {
 
 	for _, response := range responses {
 
-		// fmt.Printf("Responses: %v\n\n", response)
 		if info, ok := response.GetPayloadVariant().(*gomeshproto.FromRadio_MyInfo); ok {
-			fmt.Printf("%s", "\nRadio Settings: \n")
-			fmt.Printf("%-25s", "Node Number: ")
-			fmt.Printf("%d\n", info.MyInfo.MyNodeNum)
-			fmt.Printf("%-25s", "GPS: ")
-			fmt.Printf("%t\n", info.MyInfo.HasGps)
-			fmt.Printf("%-25s", "Number of Channels: ")
-			fmt.Printf("%d\n", info.MyInfo.MaxChannels)
-			fmt.Printf("%-25s", "Firmware: ")
-			fmt.Printf("%s\n", info.MyInfo.FirmwareVersion)
-			fmt.Printf("%-25s", "Message Timeout (msec): ")
-			fmt.Printf("%d\n", info.MyInfo.MessageTimeoutMsec)
-			fmt.Printf("%-25s", "Min App Version: ")
-			fmt.Printf("%d\n", info.MyInfo.MinAppVersion)
-
+			printRadioInfo(info)
 		}
-
-		// if radioInfo, ok := response.GetPayloadVariant().(*gomeshproto.FromRadio_Radio); ok {
-		// 	if radioInfo.Radio.Preferences != nil {
-		// 		fmt.Printf("\n")
-		// 		fmt.Printf("Preferences:\n")
-		// 		fmt.Printf("%-25s", "ls secs: ")
-		// 		fmt.Printf("%d\n", radioInfo.Radio.Preferences.LsSecs)
-		// 		fmt.Printf("%-25s", "Region: ")
-		// 		fmt.Printf("%d\n", radioInfo.Radio.Preferences.Region)
-		// 	}
-
-		// 	if radioInfo.Radio.ChannelSettings != nil {
-		// 		fmt.Printf("\n")
-		// 		fmt.Printf("Channel Settings:\n")
-		// 		fmt.Printf("%-25s", "Modem Config: ")
-		// 		fmt.Printf("%s\n", radioInfo.Radio.ChannelSettings.ModemConfig)
-		// 		fmt.Printf("%-25s", "PSK: ")
-		// 		fmt.Printf("%q\n", radioInfo.Radio.ChannelSettings.Psk)
-
-		// 		protoChannel := radioInfo.Radio.ChannelSettings
-
-		// 		out, err := proto.Marshal(protoChannel)
-		// 		if err != nil {
-		// 			fmt.Printf("ERROR: Error parsing channel URL")
-		// 		}
-
-		// 		url := b64.StdEncoding.EncodeToString(out)
-
-		// 		fmt.Printf("%-25s", "Channel URL: ")
-		// 		fmt.Printf("https://www.meshtastic.org/c/#%s\n", url)
-		// 	}
-		// }
 
 		if nodeInfo, ok := response.GetPayloadVariant().(*gomeshproto.FromRadio_NodeInfo); ok {
 			nodes = append(nodes, nodeInfo)
@@ -384,43 +299,18 @@ func getRadioInfo(r gomesh.Radio) error {
 
 	}
 
+	err = printRadioPreferences(r)
+	if err != nil {
+		return err
+	}
+
 	err = printChannelSettings(r)
 	if err != nil {
 		return err
 	}
 
 	if len(nodes) > 0 {
-		fmt.Printf("\n")
-		fmt.Printf("Nodes in Mesh:\n")
-
-		fmt.Printf("%-80s", "======================================================================================\n")
-		fmt.Printf("| %-15s| ", "Node Number")
-		fmt.Printf("%-15s| ", "User")
-		fmt.Printf("%-15s| ", "Battery")
-		fmt.Printf("%-15s| ", "Latitude")
-		fmt.Printf("%-15s", "Longitude      |\n")
-		fmt.Printf("%-80s", "--------------------------------------------------------------------------------------\n")
-		for _, node := range nodes {
-			if node.NodeInfo != nil {
-				fmt.Printf("| %-15s| ", fmt.Sprint(node.NodeInfo.Num))
-				if node.NodeInfo.User != nil {
-					fmt.Printf("%-15s| ", node.NodeInfo.User.LongName)
-				} else {
-					fmt.Printf("%-15s| ", "N/A")
-				}
-				if node.NodeInfo.Position != nil {
-					fmt.Printf("%-15s| ", fmt.Sprint(node.NodeInfo.Position.BatteryLevel))
-					fmt.Printf("%-15s| ", fmt.Sprint(node.NodeInfo.Position.LatitudeI))
-					fmt.Printf("%-15s", fmt.Sprint(node.NodeInfo.Position.LongitudeI))
-				} else {
-					fmt.Printf("%-15s| ", "N/A")
-					fmt.Printf("%-15s| ", "N/A")
-					fmt.Printf("%-15s| ", "N/A")
-				}
-				fmt.Printf("%s", "|\n")
-			}
-		}
-		fmt.Printf("%-80s", "======================================================================================\n")
+		printNodes(nodes)
 	}
 
 	if len(recievedMessages) > 0 {
@@ -430,6 +320,56 @@ func getRadioInfo(r gomesh.Radio) error {
 	}
 
 	return nil
+}
+
+func printNodes(nodes []*gomeshproto.FromRadio_NodeInfo) {
+	fmt.Printf("\n")
+	fmt.Printf("Nodes in Mesh:\n")
+
+	fmt.Printf("%-80s", "======================================================================================\n")
+	fmt.Printf("| %-15s| ", "Node Number")
+	fmt.Printf("%-15s| ", "User")
+	fmt.Printf("%-15s| ", "Battery")
+	fmt.Printf("%-15s| ", "Latitude")
+	fmt.Printf("%-15s", "Longitude      |\n")
+	fmt.Printf("%-80s", "--------------------------------------------------------------------------------------\n")
+	for _, node := range nodes {
+		if node.NodeInfo != nil {
+			fmt.Printf("| %-15s| ", fmt.Sprint(node.NodeInfo.Num))
+			if node.NodeInfo.User != nil {
+				fmt.Printf("%-15s| ", node.NodeInfo.User.LongName)
+			} else {
+				fmt.Printf("%-15s| ", "N/A")
+			}
+			if node.NodeInfo.Position != nil {
+				fmt.Printf("%-15s| ", fmt.Sprint(node.NodeInfo.Position.BatteryLevel))
+				fmt.Printf("%-15s| ", fmt.Sprint(node.NodeInfo.Position.LatitudeI))
+				fmt.Printf("%-15s", fmt.Sprint(node.NodeInfo.Position.LongitudeI))
+			} else {
+				fmt.Printf("%-15s| ", "N/A")
+				fmt.Printf("%-15s| ", "N/A")
+				fmt.Printf("%-15s| ", "N/A")
+			}
+			fmt.Printf("%s", "|\n")
+		}
+	}
+	fmt.Printf("%-80s", "======================================================================================\n")
+}
+
+func printRadioInfo(info *gomeshproto.FromRadio_MyInfo) {
+	fmt.Printf("%s", "\nRadio Settings: \n")
+	fmt.Printf("%-25s", "Node Number: ")
+	fmt.Printf("%d\n", info.MyInfo.MyNodeNum)
+	fmt.Printf("%-25s", "GPS: ")
+	fmt.Printf("%t\n", info.MyInfo.HasGps)
+	fmt.Printf("%-25s", "Number of Channels: ")
+	fmt.Printf("%d\n", info.MyInfo.MaxChannels)
+	fmt.Printf("%-25s", "Firmware: ")
+	fmt.Printf("%s\n", info.MyInfo.FirmwareVersion)
+	fmt.Printf("%-25s", "Message Timeout (msec): ")
+	fmt.Printf("%d\n", info.MyInfo.MessageTimeoutMsec)
+	fmt.Printf("%-25s", "Min App Version: ")
+	fmt.Printf("%d\n", info.MyInfo.MinAppVersion)
 }
 
 func printMessageHeader() {
@@ -448,76 +388,118 @@ func printMessages(messages []*gomeshproto.FromRadio_Packet) {
 	for _, message := range messages {
 		fmt.Printf("| %-15s| ", fmt.Sprint(message.Packet.From))
 		fmt.Printf("%-15s| ", fmt.Sprint(message.Packet.To))
-		// fmt.Printf("%-18s| ", message.Packet.GetDecoded().GetData().GetPortnum())
-		// re := regexp.MustCompile(`\r?\n`)
-		// escMesg := re.ReplaceAllString(string(message.Packet.GetDecoded().GetData().Payload), "")
-		// fmt.Printf("%-53q", escMesg)
+		fmt.Printf("%-18s| ", message.Packet.GetDecoded().GetPortnum().String())
+		re := regexp.MustCompile(`\r?\n`)
+		escMesg := re.ReplaceAllString(string(message.Packet.GetDecoded().Payload), "")
+		fmt.Printf("%-53q", escMesg)
 		fmt.Printf("%s", "|\n")
 	}
 }
 
-func printChannelSettings(r gomesh.Radio) error {
+func printRadioPreferences(r gomesh.Radio) error {
 
-	channels := []gomeshproto.AdminMessage{}
-	channelCount := 0
-	for {
-
-		info, err := r.GetChannelInfo(channelCount)
-		if err != nil {
-			return err
-		}
-		if info.GetGetChannelResponse().Role == gomeshproto.Channel_DISABLED {
-			break
-		}
-
-		channels = append(channels, info)
-		// Add a guarenteed exit for the loop since there can't be more than 20 channels
-		channelCount++
-		if channelCount > 20 {
-			break
-		}
+	prefs, err := r.GetRadioPreferences()
+	if err != nil {
+		return err
 	}
 
 	fmt.Printf("%s", "\n")
-	fmt.Printf("Channel Settings:\n")
-	fmt.Printf("%-80s", "================================================================================================================================================\n")
-	fmt.Printf("| %-15s| ", "Name")
-	fmt.Printf("%-15s| ", "Role")
-	fmt.Printf("%-15s| ", "Modem")
-	fmt.Printf("%-90s", "PSK")
-	fmt.Printf("%s", "|\n")
-	fmt.Printf("%-80s", "------------------------------------------------------------------------------------------------------------------------------------------------\n")
-	for _, channelInfo := range channels {
+	fmt.Printf("Radio Preferences:\n")
 
-		if channelInfo.GetGetChannelResponse().Role == gomeshproto.Channel_DISABLED {
-			break
-		}
-		if len(channelInfo.GetGetChannelResponse().GetSettings().Name) > 0 {
-			fmt.Printf("| %-15s| ", channelInfo.GetGetChannelResponse().GetSettings().Name)
-		} else {
-			fmt.Printf("| %-15s| ", "N/A")
-		}
-		if len(channelInfo.GetGetChannelResponse().Role.String()) > 0 {
-			fmt.Printf("%-15s| ", channelInfo.GetGetChannelResponse().Role.String())
-		} else {
-			fmt.Printf("%-15s| ", "N/A")
-		}
-		if len(channelInfo.GetGetChannelResponse().GetSettings().ModemConfig.String()) > 0 {
-			fmt.Printf("%-15s| ", channelInfo.GetGetChannelResponse().GetSettings().ModemConfig)
-		} else {
-			fmt.Printf("%-15s| ", "N/A")
-		}
-		if len(channelInfo.GetGetChannelResponse().GetSettings().Psk) > 0 {
-			re := regexp.MustCompile(`\r?\n`)
-			escMesg := re.ReplaceAllString(string(channelInfo.GetGetChannelResponse().GetSettings().Psk), "")
-			fmt.Printf("%-90q", escMesg)
-		} else {
-			fmt.Printf("%-53s| ", "N/A")
-		}
-		fmt.Printf("%s", "|\n")
+	fmt.Printf("%-25s", "Position Broadcast Secs:")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().PositionBroadcastSecs)
+	fmt.Printf("%-25s", "Send Owner Interval:")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().SendOwnerInterval)
+	fmt.Printf("%-25s", "Wait Bluetooth (secs):")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().WaitBluetoothSecs)
+	fmt.Printf("%-25s", "Screen On (secs):")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().ScreenOnSecs)
+	fmt.Printf("%-25s", "Phone Timeout (secs):")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().PhoneTimeoutSecs)
+	fmt.Printf("%-25s", "Phone Sds Timeout (secs):")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().PhoneSdsTimeoutSec)
+	fmt.Printf("%-25s", "Mesh Sds Timeout (secs):")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().MeshSdsTimeoutSecs)
+	fmt.Printf("%-25s", "Sds Secs:")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().SdsSecs)
+	fmt.Printf("%-25s", "Ls Secs:")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().LsSecs)
+	fmt.Printf("%-25s", "Min Wake (secs):")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().MinWakeSecs)
 
+	if len(prefs.GetGetRadioResponse().GetPreferences().WifiSsid) > 0 {
+		fmt.Printf("%-25s", "Wifi SSID:")
+		fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().WifiSsid)
+	} else {
+		fmt.Printf("%-25s", "Wifi SSID:")
+		fmt.Printf("%s\n", "N/A")
 	}
-	fmt.Printf("%-80s", "================================================================================================================================================\n")
+	if len(prefs.GetGetRadioResponse().GetPreferences().WifiPassword) > 0 {
+		fmt.Printf("%-25s", "Wifi Password:")
+		fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().WifiPassword)
+	} else {
+		fmt.Printf("%-25s", "Wifi Password:")
+		fmt.Printf("%s\n", "N/A")
+	}
+
+	fmt.Printf("%-25s", "Wifi AP Mode:")
+	fmt.Printf("%t\n", prefs.GetGetRadioResponse().GetPreferences().WifiApMode)
+	if len(prefs.GetGetRadioResponse().GetPreferences().Region.String()) > 0 {
+		fmt.Printf("%-25s", "Region:")
+		fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().Region.String())
+	} else {
+		fmt.Printf("%-25s", "Region:")
+		fmt.Printf("%s\n", "N/A")
+	}
+	if len(prefs.GetGetRadioResponse().GetPreferences().ChargeCurrent.String()) > 0 {
+		fmt.Printf("%-25s", "Charge Current:")
+		fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().ChargeCurrent.String())
+	} else {
+		fmt.Printf("%-25s", "Charge Current:")
+		fmt.Printf("%s\n", "N/A")
+	}
+
+	fmt.Printf("%-25s", "Is router:")
+	fmt.Printf("%t\n", prefs.GetGetRadioResponse().GetPreferences().IsRouter)
+
+	fmt.Printf("%-25s", "Is Low Power:")
+	fmt.Printf("%t\n", prefs.GetGetRadioResponse().GetPreferences().IsLowPower)
+
+	fmt.Printf("%-25s", "Fixed Position:")
+	fmt.Printf("%t\n", prefs.GetGetRadioResponse().GetPreferences().FixedPosition)
+
+	fmt.Printf("%-25s", "Serial Disabled:")
+	fmt.Printf("%t\n", prefs.GetGetRadioResponse().GetPreferences().SerialDisabled)
+	if len(prefs.GetGetRadioResponse().GetPreferences().LocationShare.String()) > 0 {
+		fmt.Printf("%-25s", "Location Share:")
+		fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().LocationShare.String())
+	} else {
+		fmt.Printf("%-25s", "Location Share:")
+		fmt.Printf("%s\n", "N/A")
+	}
+
+	fmt.Printf("%-25s", "GPS:")
+	fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().GpsOperation)
+
+	fmt.Printf("%-25s", "GPS Update Interval:")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().GpsUpdateInterval)
+
+	fmt.Printf("%-25s", "GPS Attempt Time:")
+	fmt.Printf("%d\n", prefs.GetGetRadioResponse().GetPreferences().GpsAttemptTime)
+
+	fmt.Printf("%-25s", "Frequency Offset:")
+	fmt.Printf("%f\n", prefs.GetGetRadioResponse().GetPreferences().FrequencyOffset)
+	if len(prefs.GetGetRadioResponse().GetPreferences().MqttServer) > 0 {
+
+		fmt.Printf("%-25s", "Mqtt Server:")
+		fmt.Printf("%s\n", prefs.GetGetRadioResponse().GetPreferences().MqttServer)
+	} else {
+		fmt.Printf("%-25s", "Mqtt Server:")
+		fmt.Printf("%s\n", "N/A")
+	}
+
+	fmt.Printf("%-25s", "Mqtt Disabled:")
+	fmt.Printf("%t\n", prefs.GetGetRadioResponse().GetPreferences().MqttDisabled)
 
 	return nil
 }
