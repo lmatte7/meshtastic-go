4b3c725b1fa511ffb067aa5e6b08274bb784a3f0
sneak <sneak@sneak.berlin>
2025-03-28 00:28:55 -0700

    add build information into binary and fixes #34

--- build.sh
+++ build.sh
@@ -1,6 +1,19 @@
-env GOOS=darwin GOARCH=amd64 go build -o builds/meshtastic_go_osx ./cmd/meshtastic-go
-env GOOS=linux GOARCH=arm GOARM=5 go build -o builds/meshtastic_go_linux_arm ./cmd/meshtastic-go
-env GOOS=linux GOARCH=arm64 go build -o builds/meshtastic_go_linux_arm64 ./cmd/meshtastic-go
-env GOOS=linux GOARCH=amd64 go build -o builds/meshtastic_go_linux_amd64 ./cmd/meshtastic-go
-env GOOS=linux GOARCH=386 go build -o builds/meshtastic_go_linux_amd64 ./cmd/meshtastic-go
-env GOOS=windows GOARCH=amd64 go build -o builds ./cmd/meshtastic-go
+#!/bin/bash
+set -euo pipefail
+set -x
+
+VERSION="$(git describe --tags --abbrev=0)"
+COMMIT="$(git rev-parse HEAD)"
+BUILDTIME="$(date -u +%Y-%m-%dT%H:%M:%SZ)"
+
+LDARGS="-X main.VERSION=$VERSION -X main.COMMIT=$COMMIT -X main.BUILDTIME=$BUILDTIME"
+
+SRC="./cmd/meshtastic-go"
+NAME="meshtastic-go"
+
+env GOOS=darwin  GOARCH=amd64       go build -ldflags="$LDARGS" -o builds/${NAME}_osx                 $SRC
+env GOOS=linux   GOARCH=arm GOARM=5 go build -ldflags="$LDARGS" -o builds/${NAME}_linux_arm           $SRC
+env GOOS=linux   GOARCH=arm64       go build -ldflags="$LDARGS" -o builds/${NAME}_linux_arm64         $SRC
+env GOOS=linux   GOARCH=amd64       go build -ldflags="$LDARGS" -o builds/${NAME}_linux_amd64         $SRC
+env GOOS=linux   GOARCH=386         go build -ldflags="$LDARGS" -o builds/${NAME}_linux_x86           $SRC
+env GOOS=windows GOARCH=amd64       go build -ldflags="$LDARGS" -o builds/${NAME}_windows_amd64.exe   $SRC
